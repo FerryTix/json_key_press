fc1f4109c46911594782e7abd768150cbd704b66
elsholz <hendrik.lankers.hl@googlemail.com>
2021-02-16 19:15:31 +0100

    add code

---
+++ json_key_press/test.py
@@ -0,0 +1,61 @@
+from json_key_press import CompressibleObject, JSONKeyPress, LongKey, ShortKey
+import json
+
+
+class OrderCategory(CompressibleObject):
+    def compress(self, o):
+        return {
+            'apple': 'a',
+            'peach': 'p',
+            'cherry': 'c',
+        }[o]
+
+    def decompress(self, o):
+        return {
+            'a': 'apple',
+            'p': 'peach',
+            'c': 'cherry',
+        }[o]
+
+
+order = {
+    "order_uuid": int,
+    "category": OrderCategory,
+    "amount": int,
+}
+
+test_schema = {
+    "order": order,
+    "amount": int,
+    "customer_name": str,
+}
+
+test_long = {
+    "order": {
+        "order_uuid": "7234723467",
+        "amount": 50,
+        "category": "apple"
+    },
+    "amount": 10,
+    "customer_name": "Harald",
+
+}
+
+if __name__ == '__main__':
+    t = JSONKeyPress(schema=test_schema)
+    sk = t._short_keyed
+    lk = t._long_keyed
+    assert "A" in sk
+    assert "order" in lk
+    compr = t.compress(test_long)
+    decompr = t.decompress(compr)
+    assert decompr == test_long
+    la, lb = len(json.dumps(compr)), len(json.dumps(decompr))
+    print(la, lb)
+    print("Compression rate for example:", la / lb)
+    print(json.dumps(compr, indent=4))
+    print(json.dumps(decompr, indent=4))
+
+    o = t(uncompressed=test_long)
+    assert o[ShortKey("B")] == 10
+    assert o[LongKey("amount")] == 10
